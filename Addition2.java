0f39fe35662e7bf82d94ca4e8756eb9a76e8d93a
pdhanashree <dhanashree1012@gmail.com>
2018-08-24 14:50:32 +0530

    first

---
+++ Addition2.java
@@ -0,0 +1,16 @@
+import java.io.*;
+class Addition2{
+
+	int a,b,c;
+  public static void main(String args[]){
+
+System.out.println("Enter the two value");
+
+	c= a+b;
+
+  System.out.println("Addition is " + c);
+
+  }
+
+
+}
